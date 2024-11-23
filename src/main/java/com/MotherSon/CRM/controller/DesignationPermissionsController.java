37602e8ca8d9e13d09246560e1c9fccfdb2b08e9
Alexander Dung Dung <65543943+Alexander8437@users.noreply.github.com>
2024-11-23 21:40:25 +0530

    signin api added again with cors

--- src/main/java/com/MotherSon/CRM/controller/DesignationPermissionsController.java
+++ src/main/java/com/MotherSon/CRM/controller/DesignationPermissionsController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.MotherSon.CRM.security.services.DesignationPermissionsService;
 import jakarta.persistence.EntityNotFoundException;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("Motherson/crm/v1/designationPermission")
 public class DesignationPermissionsController {
 
--- src/main/java/com/MotherSon/CRM/controller/UserController.java
+++ src/main/java/com/MotherSon/CRM/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +23,7 @@ import com.MotherSon.CRM.security.services.UserService;
 import com.MotherSon.CRM.utils.JwtUtil;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("Motherson/crm/v1")
 public class UserController {
 
